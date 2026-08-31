6786266264167e249a9393bebd7f9926cef4fcb8
Marvellous <areomarvell06@gmail.com>
2026-08-31 11:14:49 +0100

    fix: update API base URL to correct endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiURL = axios.create({
-    baseURL: "https://database-dramalist.onrender.com"
+    baseURL: "https://database-dramalist-1.onrender.com"
 });
 
 export default apiURL;
